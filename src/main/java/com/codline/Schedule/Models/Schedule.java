b1769e7eafa24efde8c967fc67587d1ee899be97
awadh3030 <awadh3030@hotmail.com>
2023-05-31 15:48:48 +0400

    Problem 16

--- src/main/java/com/codline/Schedule/Models/Schedule.java
+++ src/main/java/com/codline/Schedule/Models/Schedule.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Schedule {
 
-    @Scheduled(cron = "* 0/10 9-17 * * 1-5")
+    @Scheduled(cron = "* 0 * 1-15 * *")
 
     public void scheulde01() {
 
