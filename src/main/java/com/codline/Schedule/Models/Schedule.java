b2a75e3f35e66673ef953b7239a032d39c050319
awadh3030 <awadh3030@hotmail.com>
2023-05-31 13:31:10 +0400

    Problem 1

---
+++ src/main/java/com/codline/Schedule/Models/Schedule.java
@@ -0,0 +1,16 @@
+package com.codline.Schedule.Models;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schedule {
+
+    @Scheduled(cron = "0 */5 * * * *")
+
+    public void scheulde01() {
+
+        System.out.print("Hello Word");
+
+    }
+}
